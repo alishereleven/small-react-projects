62d9131215cade1a3ebfd7117d718283ae2815fd
alishereleven <alisherashim11@gmail.com>
2022-05-30 21:31:22 +0900

    Updated Birthday Reminder Project
    Removed unnecessary literals and added key prop

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <main>
       <section className='container'>
-        <h3>{`${people.length} birthdays today`}</h3>
+        <h3>{people.length} birthdays today</h3>
 
         <List people={people} />
 
--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const List = (props) => {
   return (
-    props.people.map(person => (
-      <div className='person'>
-        <img src={person.image} alt={person.name} />
-        <div>
-          <h4>{person.name}</h4>
-          <p>{`${person.age} years`}</p>
+    <>
+      {props.people.map(person => (
+        <div className='person' key={person.id}>
+          <img src={person.image} alt={person.name} />
+          <div>
+            <h4>{person.name}</h4>
+            <p>{person.age} years</p>
+          </div>
         </div>
-      </div>
-    ))
+      ))}
+    </>
   )
 };
 
