fb3f48954201070f671844018a82365df19d70f3
alishereleven <alisherashim11@gmail.com>
2022-05-30 21:18:02 +0900

    Uploaded Birthday Reminder Project
    Uploaded completed Birthday Reminder project

---
+++ birthday-reminder/src/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const List = (props) => {
+  return (
+    props.people.map(person => (
+      <div className='person'>
+        <img src={person.image} alt={person.name} />
+        <div>
+          <h4>{person.name}</h4>
+          <p>{`${person.age} years`}</p>
+        </div>
+      </div>
+    ))
+  )
+};
+
+export default List;
