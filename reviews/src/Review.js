9d44bc8036de39e6dbb784556fec49d312e3d8ee
alishereleven <alisherashim11@gmail.com>
2022-06-01 19:59:16 +0900

    Uploaded Review Project
    Almost completed Review project. Need to add several icons

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -3,7 +3,40 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [id, setId] = useState(people[0].id)
+  const { name, job, image, text } = people[id - 1]
 
-export default Review;
+  function prevP() {
+    setId(prevId => (
+      prevId === 1 ? people.length : prevId - 1
+    ))
+  }
+
+  function nextP() {
+    setId(prevId => (
+      prevId === people.length ? 1 : prevId + 1
+    ))
+  }
+
+  function randP() {
+    setId(Math.ceil(Math.random() * people.length))
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} className='person-img'/>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div>
+        <button className='prev-btn' onClick={prevP}>{`<`}</button>
+        <button className='next-btn' onClick={nextP}>{`>`}</button>
+      </div>
+      <button className='random-btn' onClick={randP}>surprise me</button>
+    </article>
+  )
+}
+
+export default Review
