addc194f89e6e4edb4dc5c7f62a1c6cda23848f7
alishereleven <alisherashim11@gmail.com>
2022-06-29 22:29:46 +0900

    Uploaded Finished Slider Project
    Added slider functionality

--- slider/src/App.js
+++ slider/src/App.js
@@ -4,7 +4,25 @@ import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
 
 function App() {
+  const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 7500)
+    return () => clearInterval(interval)
+  }, [])
+  
+  function prevSlide() {
+    setIndex(prevIndex => (
+      prevIndex > 0 ? prevIndex - 1 : people.length - 1
+    ))
+  }
+
+  function nextSlide() {
+    setIndex(prevIndex => (
+      prevIndex < people.length - 1 ? prevIndex + 1 : 0
+    ))
+  }
 
   return (
     <main>
@@ -13,17 +31,32 @@ function App() {
           <h2><span>/</span>Reviews</h2>
         </header>
         <div className='section-center'>
-          <article className='activeSlide'>
-            <img src={data[index].image} className='person-img' />
-            <h4>{data[index].name}</h4>
-            <p className='title'>{data[index].title}</p>
-            <p className='text'>{data[index].quote}</p>
-            <FaQuoteRight className='icon'/>
-          </article>
-          <button className='prev'>
+          {people.map((person, personIndex) => {
+            const { id, image, name, title, quote } = person
+
+            let position = 'nextSlide'
+
+            if (personIndex === index) {
+              position = 'activeSlide'
+            } else if (personIndex === (index > 0 ? index - 1 : people.length - 1)) {
+              position = 'lastSlide'
+            }
+
+            return (
+              <article key={id} className={position}>
+                <img src={image} className='person-img' />
+                <h4>{name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon'/>
+              </article>
+            )
+          })
+          }
+          <button className='prev' onClick={prevSlide}>
             <FiChevronLeft />
           </button>
-          <button className='next'>
+          <button className='next' onClick={nextSlide}>
             <FiChevronRight />
           </button>
         </div>
